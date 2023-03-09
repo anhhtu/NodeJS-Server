62772766a77795879f3e14fc45d68e3082be7036
anhhtu <vuanhtu0711@gmail.com>
2023-03-09 21:52:18 +0700

    update

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
+const httpServer = require("http").createServer(app);
+const options = { /* ... */ };
+const io = require("socket.io")(httpServer, options);
+
+io.on("connection", socket => { /* ... */ });
 
 app.get('/', function(req, res) {
     res.status(200).send('Hello world');
